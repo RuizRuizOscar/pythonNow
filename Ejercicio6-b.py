9653a39c24a3c1d327275aec349f37dccfd9da58
RuizRuizOscar <oscar.ruizruiz@gmail.com>
2021-02-20 21:08:45 -0600

    ADD Ejercicio6-b.py

---
+++ Ejercicio6-b.py
@@ -0,0 +1,27 @@
+def make_box(number):
+  """
+  INPUT
+  Esta función recibe un número entero positivo mayor a 1
+  OUTPUT
+  Si recibe algo distinto a un número entero positivo mayor a 1, retorna None
+  Si recibe un "valor"=número entero positivo mayor a 1, imprime una caja con base y lado de longitud "valor"
+  """
+  if not isinstance(number,int):
+    return None
+  if number < 1:
+    return None
+  box_list = []
+  if number == 1:
+    box_list.append("#")
+  else:
+    base, lado = "", ""
+    base = base.zfill(number).replace("0", "#")
+    box_list.append(base)
+    lado = "#" + lado.zfill(number-2).replace("0", " ") + "#"
+    i=0
+    while i < number-2:
+      box_list.append(lado)
+      i += 1
+    box_list.append(base)
+  for i in box_list:
+    print(i)
