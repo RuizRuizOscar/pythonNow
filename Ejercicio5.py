b64f8ae131f22f369bf5834ec96b1328a676d27c
RuizRuizOscar <oscar.ruizruiz@gmail.com>
2021-02-20 18:08:51 -0600

    ADD Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,19 @@
+def pluralize(lista_original):
+  """
+  INPUT
+  Esta función recibe una lista de strings
+  OUTPUT
+  Esta función retorna una lista con los strings sin repetir, y los que haya más de 1 se agrega una s
+  """
+  if not isinstance(lista_original,list):
+    return None, "No se recibió una lista"
+  for i in lista_original:
+    if not isinstance(i,str):
+      return None, "hay elementos en la lista que no son strings"
+  lista_plural = []
+  for i in lista_original:
+    if lista_plural.count(i) == 0:
+      lista_plural.append(i)
+    elif lista_plural.count(i) > 0:
+      lista_plural[lista_plural.index(i)] += "s"
+  return lista_plural
